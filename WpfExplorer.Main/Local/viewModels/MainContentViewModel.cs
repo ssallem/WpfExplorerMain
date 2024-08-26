e04e8c1f0bce74826936822f445b611acd6dca32
ssallem <ssallem@nate.com>
2024-08-26 22:07:52 +0900

    ICommand Event Binding 연결

--- WpfExplorer.Main/Local/viewModels/MainContentViewModel.cs
+++ WpfExplorer.Main/Local/viewModels/MainContentViewModel.cs
@@ -1,15 +1,27 @@
-﻿using WpfExplorer.Support.Local.Helpers;
+﻿using CommunityToolkit.Mvvm.Input;
+using System.Windows;
+using System.Windows.Input;
+using WpfExplorer.Support.Local.Helpers;
 using WpfExplorer.Support.Local.Models;
 
 namespace WpfExplorer.Main.Local.viewModels
 {
     public class MainContentViewModel
     {
-        public List<FolderInfo> Roots { get; set; }
+        public ICommand FolderChangedCommand { get; init; }
+
+        public List<FolderInfo> Roots { get; init; }
 
         public MainContentViewModel(FileService fileService)
         {
+            FolderChangedCommand = new RelayCommand<FolderInfo>(FolderChanged);
+
             Roots = fileService.GenerateRootNodes();
+        }
+
+        private void FolderChanged(FolderInfo? info)
+        {
+            MessageBox.Show($"Selected : {info!.Name}");
         }
     }
 }
